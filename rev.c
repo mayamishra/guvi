fffe63ab4b10f1c4354b5fcbf7f72a26b5747614
mayamishra <51689304+mayamishra@users.noreply.github.com>
2019-06-11 10:44:49 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int main()
+{
+    char str[100000],rev[100000];
+    scanf("%s",str);
+    for(int i=strlen(str)-1,j=0;i>=0,j<=strlen(str)-1;i--,j++)
+    {
+        rev[aj]=str[i];
+        printf("%c",rev[j]);
+    }
+    
+    
+}
