e36da26a64dc2f88e3c7e448a3dddc883614395f
mayamishra <51689304+mayamishra@users.noreply.github.com>
2019-06-11 10:29:51 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int main()
+{
+    int n1,n2,n3;
+    scanf("%d %d %d",&n1,&n2,&n3);
+    int max;
+    max=n1;
+    if(n2>max&&n3>max)
+    {
+        if(n2>n3)
+        max=n2;
+        else
+        max=n3;
+    }
+  /*  if(n2<min&&n3<min)
+    {
+        if(n2<n3)
+        min=n2;
+        else
+        min=n3;
+    }*/
+    printf("%d",max);
+}
